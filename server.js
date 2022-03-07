963cdb47ee33dd39db053b6e082aeb806744aac6
OlleNord <72620544+OlleNord@users.noreply.github.com>
2022-03-07 23:40:00 +0100

    1st commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {cors: {origin: "*"}});
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+server.listen(port, (err, res) => {
+    console.log("Server listening on port: " + port);
+});
+
+let socket_ids = [];
+
+io.on("connection", (socket) => {
+    socket_ids.push(socket.id);
+    console.log(socket_ids)
+    io.emit("id", JSON.stringify(socket_ids));
+    socket.on("clear-all", (id) => {
+        socket_ids = [];
+        io.emit("id", JSON.stringify(socket_ids));
+    });
+});
